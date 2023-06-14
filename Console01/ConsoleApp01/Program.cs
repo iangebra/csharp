fa33c58a39f738af3d9ce65939e1c86415f6103b
Ivan <ange987@gmail.com>
2023-06-14 13:02:36 +0200

    Initial commit

---
+++ Console01/ConsoleApp01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ivan");
+        }
+    }
+}
