f4e574f0c66dbdf4679fa0587f861e5f6cd8179d
Ivan <ange987@gmail.com>
2023-06-19 12:49:59 +0200

    update

---
+++ Console02/s nastave/Program.cs
@@ -0,0 +1,70 @@
+﻿
+// operatori uspoređivanja -> bool
+
+bool razlicito = 3 != 4;
+
+
+bool vece = 8 > 6;
+
+Console.WriteLine("{0},{1}", razlicito, vece);
+
+// Logički operatori -> učimo kod if naredbe
+
+bool rez = razlicito & vece;
+
+// operator + ima dvojaku ulogu
+
+string s = "Edunova" + " Osijek"; //nadoljepiti
+
+int x = 6 + 2; // zbrojiti
+
+string s1 = "Broj " + 5;
+
+// operator modulo ostatak nakon cjelobrojnog djeljenja
+// školski primjer je parni i nemarni broj
+
+// Za uneseni broj ispiši True ako je parni ili False ako je neparni
+
+Console.Write("Unesi broj: ");
+x = Int16.Parse(Console.ReadLine());
+
+Console.WriteLine(x % 2 == 0);
+
+
+// Deklarirajte varijablu tipa int naziva negativniBroj i 
+// dodjelite joj vrijednost -262
+
+int negativniBroj = -262;
+
+
+
+// ispišite pozitivni ekvivalent negativniBroj
+
+negativniBroj = negativniBroj / -1;
+Console.WriteLine(negativniBroj);
+
+
+
+
+// Za unesena dva cijela broja
+// ispišite rezutat djeljenja
+// npr. ulaz 5 i 10, izlaz 0,5 
+
+Console.Write("Unesite prvi broj : ");
+double broj1 = double.Parse(Console.ReadLine());
+Console.Write("Unesite prvi broj : ");
+double broj2 = double.Parse(Console.ReadLine());
+double rezultat = broj1 / broj2;
+Console.WriteLine(rezultat);
+
+
+// DZ
+// Za uneseni dvoznamenkasti broj
+// ispišite jediničnu vrijednost
+// unos 21, ispis 1
+// unos 87, ispis 7
+
+
+
+
+
