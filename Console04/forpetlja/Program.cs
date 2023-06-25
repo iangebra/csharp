2f04945389e34f88311b439ac6379a04e0603700
Ivan <ange987@gmail.com>
2023-06-25 20:46:01 +0200

    vjezba

---
+++ Console04/forpetlja/Program.cs
@@ -0,0 +1,31 @@
+﻿// 1. zadatak
+// Korisnik unosi dva cijela broja
+// Program ispisuje 
+// zbroj svih parnih brojeva
+// između dva unesena broja
+
+Console.Write("unesi 1. broj ");
+int br1 = int.Parse(Console.ReadLine());
+Console.Write("unesi 2. broj ");
+int br2 = int.Parse(Console.ReadLine());
+
+int manji = br1 < br2 ? br1 : br2;
+int veci = br1 > br2 ? br1 : br2;
+int zbroj = 0;
+for (int i = manji; i <= veci; i++)
+{
+    zbroj = i % 2 == 0 ? zbroj + i : zbroj;
+}
+Console.WriteLine(zbroj);
+
+
+// 2. zadatak
+// Program ispisuje brojeve od 100 do 1
+// u istom redu odvojeno zarezom
+
+for (int i = 100; i > 0; i--)
+{
+    Console.Write(i + (i != 1 ? "," : ""));
+}
+
+
---
+++ Console04/zadaca/Program.cs
@@ -0,0 +1,102 @@
+﻿
+
+Console.WriteLine("----------------------");
+Console.WriteLine(": : :  TABLICA  MNOZENJA  : : :");
+
+Console.WriteLine("----------------------");
+Console.Write("* |");
+for (int i = 1; i <= 9; i++)
+{
+
+    Console.Write(" {0}",i);
+
+}
+Console.WriteLine();
+Console.WriteLine("----------------------");
+Console.Write("1 |");
+for (int i = 1; i <= 9; i++)
+{
+
+    Console.Write(" {0}", i);
+
+}
+Console.WriteLine();
+Console.Write("2 |");
+for (int i = 2; i <= 18; i = i + 2)
+{
+
+    Console.Write(" {0}", i);
+
+}
+Console.WriteLine();
+Console.Write("3 |");
+for (int i = 3; i <= 27; i = i + 3)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.Write("4 |");
+for (int i = 4; i <= 36; i = i + 4)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.Write("5 |");
+for (int i = 5; i <= 45; i = i + 5)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.Write("6 |");
+for (int i = 6; i <= 54; i = i + 6)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.Write("7 |");
+for (int i = 7; i <= 63; i = i + 7)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.Write("8 |");
+for (int i = 8; i <= 72; i = i + 8)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.Write("9 |");
+for (int i = 9; i <= 81; i = i + 9)
+{
+
+    Console.Write(" {0}", i);
+
+
+}
+Console.WriteLine();
+Console.WriteLine("----------------------");
+Console.WriteLine(":  :  :  :  :  :   :by Ivan");
+Console.WriteLine("----------------------");
+
+
+
+
+
