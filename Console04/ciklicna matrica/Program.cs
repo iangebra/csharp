371b1e441044e516d5dedd294b1b83b3e2ab8cb4
Ivan <ange987@gmail.com>
2023-06-27 21:15:00 +0200

    update zadaca

---
+++ Console04/ciklicna matrica/Program.cs
@@ -0,0 +1,80 @@
+﻿
+int redaka = 5, stupaca = 5;
+
+//Console.Write("Unesi broj redova: ");
+//redaka = int.Parse(Console.ReadLine());
+
+//Console.Write("Unesi broj stupaca: ");
+//stupaca = int.Parse(Console.ReadLine());
+
+int[,] matrica = new int[redaka, stupaca];
+
+// pripremljena je tablica, sve su 0
+for (int i = 0; i < redaka; i++)
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("***********************");
+
+
+// treba mi brojač
+int b = 1;
+
+//matrica[redaka - 1, stupaca - 1] = b++;
+//matrica[redaka - 1, stupaca - 2] = b++;
+//matrica[redaka - 1, stupaca - 3] = b++;
+//matrica[redaka - 1, stupaca - 4] = b++;
+//matrica[redaka - 1, stupaca - 5] = b++;
+//matrica[redaka - 1, stupaca - 6] = b++; -> OVO JE GREŠKA za 5x5
+
+for (int i = 1; i <= stupaca; i++)
+{
+    matrica[redaka - 1, stupaca - i] = b++;
+}
+
+for (int i = 0; i < redaka; i++)
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("***********************");
+
+
+for (int i = redaka - 2; i >= 0; i--)
+{
+    matrica[i, 0] = b++;
+}
+
+
+for (int i = 0; i < redaka; i++)
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("***********************");
+
+
+for (int i = 1; i <= stupaca; i++)
+{
+    matrica[redaka - 2, stupaca - i] = b--;
+}
+
+for(int i = 0; i < redaka; i++)
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("***********************");
--- Console04/zadaca/Program.cs
+++ Console04/zadaca/Program.cs
@@ -1,187 +1,28 @@
-﻿Console.WriteLine("----------------------");
-Console.WriteLine(": : :  TABLICA  MNOZENJA  : : :");
-
-Console.WriteLine("----------------------");
-Console.Write("* |");
-for (int i = 1; i <= 9; i++)
-{
-
-    Console.Write(" {0}",i);
-
-}
-Console.WriteLine();
-Console.WriteLine("----------------------");
-Console.Write("1 |");
-for (int i = 1; i <= 9; i++)
-{
-
-    Console.Write(" {0}", i);
-
-}
-Console.WriteLine();
-Console.Write("2 |");
-for (int i = 2; i <= 18; i = i + 2)
-{
-
-    Console.Write(" {0}", i);
-
-}
-Console.WriteLine();
-Console.Write("3 |");
-for (int i = 3; i <= 27; i = i + 3)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.Write("4 |");
-for (int i = 4; i <= 36; i = i + 4)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.Write("5 |");
-for (int i = 5; i <= 45; i = i + 5)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.Write("6 |");
-for (int i = 6; i <= 54; i = i + 6)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.Write("7 |");
-for (int i = 7; i <= 63; i = i + 7)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.Write("8 |");
-for (int i = 8; i <= 72; i = i + 8)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.Write("9 |");
-for (int i = 9; i <= 81; i = i + 9)
-{
-
-    Console.Write(" {0}", i);
-
-
-}
-Console.WriteLine();
-Console.WriteLine("----------------------");
-Console.WriteLine(":  :  :  :  :  :   :by Ivan");
-Console.WriteLine("----------------------");
-
-
-
-
-
-
-
-
-
-
-
+﻿Console.Write("Upisi ime: ");
+string ime = Console.ReadLine();
 string s;
-
-for (int i = 1; i <= 9; i++)
+Console.WriteLine("-------------------------------");
+Console.WriteLine(": : :  TABLICA  MNOZENJA  : : :");
+Console.WriteLine("-------------------------------");
+Console.Write(" * |");
+for (int i = 1;i < 10; i++)
 {
-    s = "    " + "-";
-    Console.Write(s[^4..]);
-
-
+    Console.Write("  " + i);
 }
 Console.WriteLine();
-
-
-for (int k = 1; k <= 9; k++)
+Console.WriteLine("-------------------------------");
+for (int j = 1; j < 10; j++)
+{
+    Console.Write(" " + j + " |");
+    for (int k = 1; k < 10; k++)
     {
-        s = "    " +  k;
-        Console.Write(s[^4..]);
+        s = "   " + j * k;
+        Console.Write(s[^3..]);
     }
     Console.WriteLine();
-
-for (int k = 2; k <= 18; k = k + 2)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
 }
+Console.WriteLine("-------------------------------");
+s = ":  :  :  :  :  :  :  :  :  : by " + ime;
+Console.Write(s[^31..]);
 Console.WriteLine();
-for (int k = 3; k <= 27; k = k + 3)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-for (int k = 4; k <= 36; k = k + 4)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-for (int k = 5; k <= 45; k = k + 5)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-for (int k = 6; k <= 54; k = k + 6)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-for (int k = 7; k <= 63; k = k + 7)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-for (int k = 8; k <= 72; k = k + 8)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-for (int k = 9; k <= 81; k = k + 9)
-{
-    s = "    " + k;
-    Console.Write(s[^4..]);
-}
-Console.WriteLine();
-
-
-for (int i = 1; i <= 9; i++)
-{
-    s = "    " + "-";
-    Console.Write(s[^4..]);    
-}
-Console.WriteLine();
+Console.WriteLine("-------------------------------");
