6f6b060f9696c73fb6d8fd83a12b7a63c9153af1
Ivan <ange987@gmail.com>
2023-07-03 13:34:23 +0200

    matrica gotova

--- Console04/ciklicna matrica/Program.cs
+++ Console04/ciklicna matrica/Program.cs
@@ -1,103 +1,71 @@
-﻿
-int redaka = 5, stupaca = 5;
-
-//Console.Write("Unesi broj redova: ");
-//redaka = int.Parse(Console.ReadLine());
-
-//Console.Write("Unesi broj stupaca: ");
-//stupaca = int.Parse(Console.ReadLine());
+﻿int redaka = 5, stupaca = 5;
 
 int[,] matrica = new int[redaka, stupaca];
 
-// pripremljena je tablica, sve su 0
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
-
-
-// treba mi brojač
 int b = 1;
+string razmak;
 
-//matrica[redaka - 1, stupaca - 1] = b++;
-//matrica[redaka - 1, stupaca - 2] = b++;
-//matrica[redaka - 1, stupaca - 3] = b++;
-//matrica[redaka - 1, stupaca - 4] = b++;
-//matrica[redaka - 1, stupaca - 5] = b++;
-//matrica[redaka - 1, stupaca - 6] = b++; -> OVO JE GREŠKA za 5x5
-
-
-
-
-    for (int i = 1; i <= stupaca; i++)
+//lijevo
+for (int i = 1; i <= stupaca; i++)
 {
     matrica[redaka - 1, stupaca - i] = b++;
 }
 
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
-
-
+//gore
 for (int i = redaka - 2; i >= 0; i--)
 {
     matrica[i, 0] = b++;
 }
 
-
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
-
-
+//desno
 for (int i = 1; i <= stupaca-1; i++)
    
     {
     matrica[0,i] = b++;
 }
 
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("***********************");
-
-
-
+//dole
 for (int i = 1; i <= stupaca - 2; i++)
 {
     matrica[i, stupaca - 1] = b++;
 }
+
+//opet lijevo
+for (int i = 3; i >= stupaca - 4; i--)
+{
+    matrica[3, i] = b++;
+}
+
+//opet gore
+for (int i = 2; i >= redaka - 4; i--)
+{
+    matrica[i, 1] = b++;
+}
+
+//opet desno
+for (int i = 2; i <= stupaca - 2; i++)
+{
+    matrica[1, i] = b++;
+}
+
+//opet dole
+for (int i = 2; i <= stupaca - 3; i++)
+{
+    matrica[i, stupaca - 2] = b++;
+}
+
+//jos jednom lijevo i ispis
+
+for (int i = 3; i < stupaca-1; i++)
+{
+    matrica[redaka - 3, stupaca - i] = b++;
+}
 for (int i = 0; i < redaka; i++)
 {
     for (int j = 0; j < stupaca; j++)
     {
-        Console.Write(matrica[i, j] + " ");
+        razmak = "   " + matrica[i, j];
+        Console.Write(razmak[^4..]);
     }
     Console.WriteLine();
 }
-Console.WriteLine("***********************");
-
-
-
